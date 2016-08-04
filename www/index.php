c01715e1982dcf2fb13caa6f6a52a9a727468e77
Toby Samples <toby.samples@gmail.com>
2016-08-04 13:28:18 -0500

    Test

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>PSC Listens Hi Marky!</title>
+	<title>PSC Listens</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
